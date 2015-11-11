ee875620d739e797cd58818ca9741a3fe701f99a
Matthew Summers <matt@wishf.co.uk>
2015-11-11 22:47:50 +0000

    Belated initial commit

---
+++ app/src/main/java/io/bleepr/floor/bleepriofloormanagement/APIOrdersAdapter.java
@@ -0,0 +1,30 @@
+package io.bleepr.floor.bleepriofloormanagement;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Matthew on 10/11/2015.
+ */
+public class APIOrdersAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ app/src/main/java/io/bleepr/floor/bleepriofloormanagement/OrdersAPIWrapper.java
@@ -0,0 +1,7 @@
+package io.bleepr.floor.bleepriofloormanagement;
+
+/**
+ * Created by Matthew on 10/11/2015.
+ */
+public class OrdersAPIWrapper {
+}
